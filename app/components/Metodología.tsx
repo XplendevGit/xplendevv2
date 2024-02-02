386ef0bc24e9eaf2a831a63f18c2d9e0149b08f8
Luis Rivera <l.riveraa664@hotmail.com>
2024-02-02 04:07:36 -0300

    Updates

--- app/components/Metodología.tsx
+++ app/components/Metodología.tsx
@@ -1,8 +1,8 @@
 "use client"
 import React from 'react'
-import { CiBoxList, CiGrid42 } from "react-icons/ci";
-import { FaLaptopCode } from "react-icons/fa";
-import { MdVerifiedUser } from "react-icons/md";
+import { CiGrid42 } from "react-icons/ci";
+import { MdGroupAdd, MdVerifiedUser, MdOutlinePublishedWithChanges } from "react-icons/md";
+import { IoCloudUpload } from "react-icons/io5";
 import Link from 'next/link';
 
 import { useEffect } from 'react';
@@ -36,36 +36,43 @@ function Metodología({}: Props) {
 
             <div className="contenedor-metodologia h-full">
 
-                    <div className="metodologia-1 grid">
+                    <div className="metodologia-1 flex flex-col md:pr-12 text-center lg:text-start">
 
-                        <div className="lg:py-12 md:py-2 py-2 xl:space-y-8 space-y-4 no-select">
+                        <div className="lg:py-12 md:py-2 py-2 xl:space-y-8 space-y-2 no-select">
                           
 
-                          <div className="grid justify-center lg:justify-start">
+                          <div className="flex flex-col justify-center lg:justify-start">
 
-                          <h1 className="xl:text-6xl lg:text-4xl md:text-3xl text-xl font-hammersmithOne text-white transition-all duration-500">
-                          Construyendo Tu Mundo Digital
+                          <h1 className="xl:text-6xl lg:text-4xl md:text-3xl text-xl font-hammersmithOne transition-all duration-500 text-[#29F2CD]">
+                          SiteWizard CMS<br></br> <span className="text-white">Conoce Nuestro Gestor de contenido</span>
                           </h1>
                            
                           </div>
 
-                           <div className="grid justify-center text-center lg:text-start lg:justify-start">
+                           <div className="flex flex-col justify-center text-center lg:text-start lg:justify-start space-y-2">
 
                            <p className="xl:text-2xl lg:text-xl text-lg font-unicaone text-[#29F2CD] transition-all duration-500 uppercase">
-                            Tu web está Completa y Segura con nosotros.<br></br>Ofrecemos asesoramiento y contratación de Hosting y Dominios .com/.cl de calidad, proporcionándole seguridad y alta velocidad de respuesta de tu sitio web.
-                            <br /><span className="text-white">¿Quieres saber más?</span></p>
+                            A través de nuestros planes ofrecemos la integración de nuestro administrador de contenido SiteWizard CMS, para que logres administrar el contenido de tu sitio desde cualquier lugar de manera rápida y sencilla.
+                            </p>
 
-                           </div>
-                          
-                              <div className="flex justify-center xl:justify-start pt-5 lg:pt-2">
+                            <p className="xl:text-2xl lg:text-xl text-lg font-unicaone text-white transition-all duration-500 uppercase">¿Quieres Saber más?</p>
 
-                              <Link href={'/mundodigital'}>
+                            <div className="w-full h-auto flex pt-2 justify-center lg:justify-start">
+
+                            <Link href={'/mundodigital'}>
                               <button className="text-white hover:border-[#F8368F] hover:text-[#F8368F] transition-all hover:scale-105 duration-500 lg:text-xl font-unicaone no-select px-4 py-2 rounded-full border-2 uppercase">
-                                Conoce más detalles
+                                Ir al sitio de SiteWizard CMS
                               </button>
                               </Link>
 
-                              </div>
+                            </div>
+
+
+
+                           </div>
+                      
+
+
 
                         </div>
                       
@@ -76,17 +83,17 @@ function Metodología({}: Props) {
                         <div className="xl:p-12 md:p-4 p-2 space-y-2">
 
                           <div className="grid md:justify-start justify-center">     
-                        <CiBoxList className="h-20 w-20 text-white myfilter transition-all duration-500 hover:translate-y-[-8%] hover:scale-105" />
+                        <MdVerifiedUser className="h-20 w-20 text-white myfilter transition-all duration-500 hover:translate-y-[-8%] hover:scale-105" />
                           </div>
 
                           <div className="grid md:justify-start justify-center">
                           <h1 className="xl:text-2xl lg:text-xl md:text-lg text-md font-hammersmithOne text-white transition-all duration-500 myfilter">
-                          Análisis
+                          Acceso Seguro
                           </h1>
                           </div>
 
                           <p className="xl:text-xl lg:text-xl text-lg font-unicaone text-white transition-all duration-500 uppercase myfilter">
-                          estudiamos tus necesidades y objetivos para diseñar un modelo digital personalizado</p>
+                          Accedes directamente a SiteWizard CMS con tus credenciales</p>
 
                         </div>
 
@@ -98,19 +105,19 @@ function Metodología({}: Props) {
 
                       <div className="grid md:justify-start justify-center">
 
-                      <CiGrid42 className="h-20 w-20 text-white myfilter transition-all duration-500 hover:translate-y-[-8%] hover:scale-105" />
+                      <MdOutlinePublishedWithChanges className="h-20 w-20 text-white myfilter transition-all duration-500 hover:translate-y-[-8%] hover:scale-105" />
                       </div>
 
                       <div className="grid md:justify-start justify-center">
                       <h1 className="xl:text-2xl lg:text-xl md:text-lg text-md font-hammersmithOne text-white transition-all duration-500 myfilter  md:pl-0 pl-[1.5vh]">
-                         Diseño y Modelado
+                      Administra el Contenido
                          </h1>
                       </div>
 
 
 
                            <p className="xl:text-xl lg:text-xl text-lg font-unicaone text-white myfilter transition-all duration-500 uppercase">
-                           Damos vida a tu visión. Creamos prototipos, definimos la estética y la arquitectura visual de tu sitio</p>
+                           Administra el contenido de tu sitio, editando el contenido de las secciones desde textos hasta imagenes</p>
 
                     </div>
 
@@ -121,18 +128,18 @@ function Metodología({}: Props) {
                     <div className="xl:p-12 md:p-4 p-2 space-y-2">
 
                       <div className="grid md:justify-start justify-center">
-                      <FaLaptopCode className="h-20 w-20 text-white transition-all duration-500 myfilter hover:translate-y-[-8%] hover:scale-105" />
+                      <MdGroupAdd className="h-20 w-20 text-white transition-all duration-500 myfilter hover:translate-y-[-8%] hover:scale-105" />
                       </div>
 
                       <div className="grid md:justify-start justify-center">
                       <h1 className="xl:text-2xl lg:text-xl md:text-lg text-md font-hammersmithOne text-white transition-all duration-500 myfilter md:pl-0 pl-[1.5vh]">
-                     Codificación de la Web
+                       Multiples Administradores
                         </h1>
                       </div>
 
 
                         <p className="xl:text-xl lg:text-xl text-lg font-unicaone text-white myfilter transition-all duration-500 uppercase">
-                        Convertimos los diseños en realidad. Utilizamos tecnologías web para construir tu sitio </p>
+                        Es posible agregar cuentas para que varios usuarios puedan administrar el contenido del sitio</p>
 
                        </div>
 
@@ -145,17 +152,17 @@ function Metodología({}: Props) {
 
                       <div className="grid md:justify-start justify-center">
                           
-                       <MdVerifiedUser className="h-20 w-20 text-white transition-all duration-500 myfilter hover:translate-y-[-8%] hover:scale-105" />
+                       <IoCloudUpload className="h-20 w-20 text-white transition-all duration-500 myfilter hover:translate-y-[-8%] hover:scale-105" />
                       </div>
 
                       <div className="grid md:justify-start justify-center">
                       <h1 className="xl:text-2xl lg:text-xl md:text-lg text-md font-hammersmithOne text-white transition-all duration-500 myfilter md:pl-0 pl-[1.5vh]">
-                        Entrega y Monitoreo
+                      Sincronización en la nube
                        </h1>
                       </div>
 
                           <p className="xl:text-xl lg:text-xl text-lg font-unicaone text-white myfilter transition-all duration-500 uppercase">
-                          Desplegamos tu sitio web y realizamos pruebas exhaustivas para garantizar su funcionamiento óptimo</p>
+                          Editas y Publicas de manera instantanea desde cualquier dispositivo conectado a la red</p>
 
                           </div>
 
