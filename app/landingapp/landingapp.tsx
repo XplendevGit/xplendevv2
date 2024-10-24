c4e80c508819ee1c3ebadc37593d2ab3c5da7b07
XplendevGit <l.riveraa664@hotmail.com>
2024-10-24 15:03:05 -0300

    updates v3.0.2

--- app/landingapp/landingapp.tsx
+++ app/landingapp/landingapp.tsx
@@ -1,4 +1,3 @@
-// pages/LandingApp.js
 import React from "react";
 
 const LandingApp = () => {
@@ -6,19 +5,16 @@ const LandingApp = () => {
     <div className="min-h-screen bg-gradient-to-b from-[#4C1D95] to-[#8B5CF6] flex flex-col items-center">
       
       {/* Header Section */}
-      <header className="text-center text-white py-10 px-6">
-        <h1 className="text-5xl font-bold">
-          Awesome App for Your Modern Lifestyle
+      <header className="text-center text-white py-16 px-6 mt-12"> {/* Ajuste de padding superior */}
+        <h1 className="text-4xl font-bold pb-4 max-w-3xl mx-auto">
+        NutriSnap Tu Compañero Inteligente para una Alimentación Saludable
         </h1>
-        <p className="mt-4 text-lg">
-          Manage your daily activities with a simple tap on your phone.
+        <p className="mt-6 text-lg max-w-2xl mx-auto"> {/* Ajuste de márgenes y ancho máximo */}
+        Gestiona tu nutrición y compara alimentos fácilmente con IA, eligiendo los mejores según tus metas y necesidades de salud.
         </p>
-        <div className="mt-8 space-x-4">
+        <div className="mt-10 space-x-4">
           <button className="bg-purple-600 text-white px-6 py-2 rounded-lg hover:bg-purple-800">
-            Download App
-          </button>
-          <button className="bg-white text-purple-600 px-6 py-2 rounded-lg hover:bg-gray-200">
-            Become a Member
+            Descargar App
           </button>
         </div>
 
@@ -36,100 +32,88 @@ const LandingApp = () => {
       <section className="text-white flex flex-wrap justify-center space-x-8 mt-12">
         <div className="text-center">
           <h2 className="text-4xl font-bold">1,265</h2>
-          <p>Users</p>
+          <p>Ususarios</p>
         </div>
         <div className="text-center">
           <h2 className="text-4xl font-bold">1,000</h2>
-          <p>Downloads</p>
+          <p>Descargas</p>
         </div>
         <div className="text-center">
           <h2 className="text-4xl font-bold">508</h2>
           <p>Likes</p>
         </div>
-        <div className="text-center">
-          <h2 className="text-4xl font-bold">809</h2>
-          <p>5-star Rating</p>
-        </div>
       </section>
 
       {/* Features Section */}
       <section className="text-white py-12 px-6 grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mt-12">
-        <div className="bg-purple-900 p-6 rounded-xl shadow-lg text-center">
+        <div className="bg-purple-900 p-8 rounded-xl shadow-lg text-center transition-all duration-200 hover:scale-105"> {/* Ajuste de padding */}
           <img
-            src="/path/to/feature1-icon.png" // Reemplaza con la URL del icono correspondiente
+            src="https://i.postimg.cc/hGXnLQSx/Nutri-icon.png" // Reemplaza con la URL del icono correspondiente
             alt="Design & Branding"
             className="w-16 h-16 mx-auto mb-4"
           />
-          <h3 className="text-2xl font-bold">Design & Branding</h3>
+          <h3 className="text-2xl font-bold">Evaluación de Objetivos Nutricionales</h3>
           <p className="mt-4">
-            A clean interface with engaging content tailored to your needs.
+          Selecciona alimentos que te ayuden a alcanzar tus metas nutricionales, como perder peso o ganar masa muscular.
           </p>
         </div>
-        <div className="bg-purple-900 p-6 rounded-xl shadow-lg text-center">
+        <div className="bg-purple-900 p-8 rounded-xl shadow-lg text-center transition-all duration-200 hover:scale-105"> {/* Ajuste de padding */}
           <img
-            src="/path/to/feature2-icon.png" // Reemplaza con la URL del icono correspondiente
+            src="https://i.postimg.cc/nLrF3w1X/AI-icon.png" // Reemplaza con la URL del icono correspondiente
             alt="Fully Secured"
             className="w-16 h-16 mx-auto mb-4"
           />
-          <h3 className="text-2xl font-bold">Fully Secured</h3>
+          <h3 className="text-2xl font-bold">Comparación de Alimentos con IA</h3>
           <p className="mt-4">
-            Protecting your data with advanced encryption and security.
+          Compara alimentos con IA y recibe recomendaciones según tus metas de salud y características personales.
           </p>
         </div>
-        <div className="bg-purple-900 p-6 rounded-xl shadow-lg text-center">
+        <div className="bg-purple-900 p-8 rounded-xl shadow-lg text-center transition-all duration-200 hover:scale-105"> {/* Ajuste de padding */}
           <img
-            src="/path/to/feature3-icon.png" // Reemplaza con la URL del icono correspondiente
+            src="https://i.postimg.cc/hGLr3pPq/infectado.png" // Reemplaza con la URL del icono correspondiente
             alt="Easy to Edit"
             className="w-16 h-16 mx-auto mb-4"
           />
-          <h3 className="text-2xl font-bold">Easy to Edit</h3>
+          <h3 className="text-2xl font-bold">Análisis Personalizado para Enfermedades</h3>
           <p className="mt-4">
-            Manage and edit your preferences and settings with ease.
+             Recibe sugerencias de alimentos basadas en tus condiciones médicas.
           </p>
         </div>
       </section>
 
       {/* How it works Section */}
       <section className="text-white py-12 px-6 max-w-6xl text-center">
-        <h2 className="text-3xl font-bold mb-8">How does this App Work?</h2>
+        <h2 className="text-3xl font-bold mb-8">¿Cómo Funciona NutriSnap?</h2>
         <div className="flex flex-wrap justify-center md:space-x-8 space-y-6 md:space-y-0">
           <div className="text-center">
             <img
-              src="/path/to/step1-icon.png" // Reemplaza con la URL del icono correspondiente
+              src="https://i.postimg.cc/XYJmmZrv/1create-step.png" // Reemplaza con la URL del icono correspondiente
               alt="Step 1"
-              className="w-12 h-12 mx-auto mb-4"
+              className="w-52 h-52 mx-auto mb-4"
             />
-            <h3 className="text-xl font-semibold">Make a Profile</h3>
-            <p>Quick setup and customization.</p>
+            <h3 className="text-xl font-semibold">1. Crear una cuenta en NutriSnap</h3>
+            <p>Regístrate fácilmente y comienza a usar la app para gestionar tu nutrición de manera inteligente.</p>
           </div>
           <div className="text-center">
             <img
-              src="/path/to/step2-icon.png" // Reemplaza con la URL del icono correspondiente
+              src="https://i.postimg.cc/fTM67qZm/2create-step.png" // Reemplaza con la URL del icono correspondiente
               alt="Step 2"
-              className="w-12 h-12 mx-auto mb-4"
+              className="w-52 h-52 mx-auto mb-4"
             />
-            <h3 className="text-xl font-semibold">Download for Free</h3>
-            <p>Available on all platforms.</p>
+            <h3 className="text-xl font-semibold">2. Completar tu perfil</h3>
+            <p>Rellena tu perfil con detalles como tu físico, condiciones de salud y objetivos nutricionales para obtener recomendaciones personalizadas.</p>
           </div>
           <div className="text-center">
             <img
-              src="/path/to/step3-icon.png" // Reemplaza con la URL del icono correspondiente
+              src="https://i.postimg.cc/15Mbv05T/3create-step.png" // Reemplaza con la URL del icono correspondiente
               alt="Step 3"
-              className="w-12 h-12 mx-auto mb-4"
+              className="w-52 h-52 mx-auto mb-4"
             />
-            <h3 className="text-xl font-semibold">Enjoy the App</h3>
-            <p>Track your activities and stay productive.</p>
+            <h3 className="text-xl font-semibold">3. Comparar alimentos con la cámara</h3>
+            <p>Utiliza la cámara para comparar alimentos en tiempo real y recibe resultados basados en inteligencia artificial, adaptados a tus necesidades.</p>
           </div>
         </div>
 
-        {/* App Mockup Image (Bottom) */}
-        <div className="mt-12">
-          <img
-            src="/path/to/mockup-image-bottom.png" // Reemplaza con la URL de la imagen mockup
-            alt="App Bottom Mockup"
-            className="w-72 mx-auto"
-          />
-        </div>
       </section>
     </div>
   );
--- app/landingapp/page.tsx
+++ app/landingapp/page.tsx
@@ -1,8 +1,26 @@
 import Landingapp from '../landingapp/landingapp'
+import Header_2 from '../components/Header_2'
+import Footer from '../components/Footer'
+import Contact from '../components/Contact'
 import React from 'react'
 
 export default function LandingAppHome(){
   return (
-    <Landingapp/>
+    <>
+
+    <Header_2/>
+
+    <section className="" id="landing">
+       <Landingapp/>
+      </section>
+
+    <section className="" id="contact">
+        <Contact/>
+      </section>
+
+    <Footer />
+
+    </>
+    
   )
 }
