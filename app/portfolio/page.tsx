4f66f45c66db20794caae5fa583fa165d40cf0f5
XplendevGit <l.riveraa664@hotmail.com>
2024-11-06 20:49:03 -0300

    updates v3.1.0

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import Contact from '../components/Contact'
 import Hero from './Hero'
 import Recomendations from './Recomendations'
+import Membership from '../components/Membership'
 
 export default function Obras(){
   return (
@@ -22,6 +23,10 @@ export default function Obras(){
 
       <section className="" id="recomendations">
        <Recomendations />
+      </section>
+
+      <section className="" id="memberships">
+        <Membership/>
       </section>
     
     <section className="" id="contact">
