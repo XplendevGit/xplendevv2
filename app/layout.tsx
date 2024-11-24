304a4ecfa24020230bc8d82de5ea2cb353947512
XplendevGito <l.riveraa664@hotmail.com>
2024-11-24 17:11:52 -0300

    updates v3.5.1 Metricol

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,47 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Xplendev',
   description: '© 2023 XplenDev | Code By Luis Rivera Araya',
-}
+};
 
-export default function RootLayout({children}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" />
-      <title>Xplendev</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        
-      </meta>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Xplendev</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        {/* Script de Metricool */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function loadScript(a){
+                var b=document.getElementsByTagName("head")[0],
+                c=document.createElement("script");
+                c.type="text/javascript";
+                c.src="https://tracker.metricool.com/resources/be.js";
+                c.onreadystatechange=a;
+                c.onload=a;
+                b.appendChild(c);
+              }
+              loadScript(function(){
+                beTracker.t({hash:"de38f1094602e400e52e7a85a6a20aff"});
+              });
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
-        <main className="mx-w-4xl mx-auto">
-        {children}
-        </main>
+        <main className="mx-w-4xl mx-auto">{children}</main>
       </body>
     </html>
-  )
+  );
 }
